d9b3e335a09c2a63734826d1bd67565eb9ae6fe7
ValentineOO <varlomxg@gmail.com>
2022-08-22 03:14:53 +0100

    added some notes to the js file.

--- script.js
+++ script.js
@@ -18,17 +18,17 @@ let weather = {
         const { speed } = data.wind;
         document.querySelector(".city").innerText = "Weather in " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-        // document.querySelector(".icon").src = " http://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidity: "  + humidity + "%";
         document.querySelector(".wind").innerText = "Wind speed: "  + speed + " m/s";
 
-        document.querySelector(".weather").classList.remove("loading")  //removing dummy text//
+        document.querySelector(".weather").classList.remove("loading") 
         document.body.style.backgroundImage = "url('https://source.unsplash.com/random/1600x900/? " + name + "')"
     },
 
-    search: function () {
+    // getting the search bar to work; getting the content of the search-bar and searching for it.
+    search: function () {                                                       
         this.fetchWeather(document.querySelector(".search-bar").value);
     }
 };
